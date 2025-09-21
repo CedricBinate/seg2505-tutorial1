453711a1cf4be6516f3fb420dd5376ecff36a67b
Cedric Binate <cedricbinate30@gmail.com>
2025-09-20 23:21:21 -0400

    Mon quatrieme commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # seg2505-tutorial1
 
 Bienvenue dans notre projet !
+gitgit -c user.name="Cedric Binate" -c user.email="cedric.binate@uottawa.ca" commit -m "Mon deuxième commit."
--- game.js
+++ game.js
@@ -3,5 +3,6 @@
 // Ou ajoutez une nouvelle fonction
 function nouvelleFeature() {
     console.log("Nouvelle fonctionnalité ajoutée");
+    console.log (" tester pour configuration de mon commit")
 }
 
